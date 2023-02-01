7066efd8a9d5c8efd7129fc7a45c4f34d0b9b4a0
Pavan R <101443408+pavankishan8@users.noreply.github.com>
2023-02-01 16:52:49 +0530

    New

---
+++ ProductWebApplication/CustomControls/Server.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Server.ascx.cs" Inherits="ProductWebApplication.CustomControls.Server" %>
+<%@ OutputCache Duration="10" VaryByParam="none" %>
+<hr />
+<div>
+<asp:Label Text="" ID="txttime" runat="server" />
+</div>
---
+++ ProductWebApplication/CustomControls/Server.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProductWebApplication.CustomControls
+{
+    public partial class Server : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txttime.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
