4b5ae49cb35875a95a9919151e4068bcc4cec69a
Pavan R <101443408+pavankishan8@users.noreply.github.com>
2023-02-02 15:30:35 +0530

    New MVC Files

---
+++ MVC App/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC_App
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Calci",
+                url: "{controller}/{action}/{p1}/{p2}",
+                defaults: new { controller = "Home", action = "GetNumber" }
+            );
+
+            routes.MapRoute(
+                name: "Employee",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "DisplayEmployee"}
+            );
+
+            routes.MapRoute(
+                name: "Home",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "HelloWorld" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Cars", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
