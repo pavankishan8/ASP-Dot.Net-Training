38a563a457a170a1c9b7bf5c1dc466e8ecbd9efd
Pavan R <101443408+pavankishan8@users.noreply.github.com>
2023-02-06 17:24:25 +0530

    New

---
+++ Employee App/EmployeeLib/DataClasses/EmployeeDataComponent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLib.DataClasses
+{
+    public class EmployeeDataComponent : IDataClasses
+    {
+        private Entities _context = new Entities();
+        public void AddNewEmployee(Employee emp)
+        {
+            _context.Employees.Add(emp);
+            _context.SaveChanges();
+        }
+
+        public void DeleteEmployee(int empId)
+        {
+            var rec = _context.Employees.FirstOrDefault((e) => e.EmpId == empId);
+            if (rec != null)
+            {
+                _context.Employees.Remove(rec);
+                _context.SaveChanges();
+            }
+            else {
+                throw new System.Exception("Employee Not Found!");
+            }
+        }
+
+        public List<Dept> GetAllDepts()
+        {
+            return _context.Depts.ToList();
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            var rec = _context.Employees.FirstOrDefault((e) => e.EmpId == emp.EmpId);
+            if (rec != null)
+            {
+                rec.Address = emp.Address;
+                rec.DeptId = emp.DeptId;
+                rec.EmpName = emp.EmpName;
+                rec.Salary = emp.Salary;
+            }
+            else
+            {
+                throw new System.Exception("Employee Not Found!");
+            }
+        }
+
+       
+    }
+}
---
+++ Employee App/EmployeeLib/DataClasses/IDataClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLib.DataClasses
+{
+    public interface IDataClasses
+    {
+        void AddNewEmployee(Employee emp);
+        void UpdateEmployee(Employee emp);
+        void DeleteEmployee(int id);
+        List<Employee> GetAllEmployees();
+        List<Dept> GetAllDepts();
+
+    }
+}
