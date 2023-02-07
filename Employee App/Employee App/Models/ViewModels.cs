7e5087ab13b8dfb0257d18d0614c1650fe999b2d
Pavan R <101443408+pavankishan8@users.noreply.github.com>
2023-02-07 15:22:35 +0530

    Updated

---
+++ Employee App/Employee App/Models/ViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeApp.Models
+{
+    public class EmployeeVM
+    {
+        public int EmpId { get; set; }
+        [Required(ErrorMessage = "Name is Mandatory")]
+        public string EmpName { get; set; }
+        [Required(ErrorMessage = "Address is Mandatory")]
+        [MinLength(4, ErrorMessage = "City name should be min of 4 characters")]
+        public string EmpAddress { get; set; }
+        [Range(20000, 100000, ErrorMessage = "Salary should be less than 100000 and more than 20000")]
+        public int EmpSalary { get; set; }
+        public int DeptId { get; set; }
+    }
+}
