38af2f84db0b41e025a245b8eb9b27860d5e1abc
Pavan R <101443408+pavankishan8@users.noreply.github.com>
2023-02-07 16:27:53 +0530

    Add files via upload

---
+++ Customer Login MVC/Login MVC/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using LoginApp.DataRepos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LoginApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string pwd)
+        {
+            var com = new UserModule();
+            try
+            {
+                var cst = com.ValidateUser(email, pwd);
+                Session["CurrentUser"] = cst;
+                FormsAuthentication.SetAuthCookie(cst.CustomerEmail, false);
+                FormsAuthentication.RedirectFromLoginPage(cst.CustomerEmail, false);
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("LoginError", ex.Message);
+                return View();
+            }
+        }
+    }
+}
---
+++ Customer Login MVC/Login MVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using LoginApp.DataComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            if (Session["CurrentUser"] != null)
+            {
+                var cstDetails = Session["CurrentUser"] as Customer;
+                return $"<h1>Home Page</h1><hr/><p>Welcome to User {cstDetails.CustomerName}";
+
+            }
+            else
+            {
+                return $"<h1>Home Page</h1><hr/><p>Welcome to Anonymous User";
+            }
+        }
+    }
+}
