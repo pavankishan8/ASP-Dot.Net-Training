8828b8b566114f6782bbacbacee136013e89e71e
Pavan R <101443408+pavankishan8@users.noreply.github.com>
2023-02-08 17:14:23 +0530

    Add files via upload

---
+++ Products Web API/WebProducts/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProducts.Models
+{
+    public class Product
+    {
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public string ProductImage { get; set; }
+            public int Price { get; set; }
+            public int Quantity { get; set; } = 1;
+
+     }
+}
