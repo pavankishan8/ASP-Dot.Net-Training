28e65bb931c76463d3a0c898b15c2478a8992780
Pavan R <101443408+pavankishan8@users.noreply.github.com>
2023-02-21 09:41:08 +0530

    New

---
+++ WebForms New/SampleAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using SampleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SampleAPI.Controllers
+{
+    [EnableCors("*","*","*","*")]
+    public class EmployeeController : ApiController
+    {
+        public List<Employee> GetAllEmployees()
+        {
+            var repo = new EmployeeDatabase();
+            return repo.GetEmployees();
+        }
+    }
+}
---
+++ WebForms New/SampleAPI/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleAPI.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public int EmpSalary { get; set; }
+    }
+
+    public class EmployeeDatabase
+    {
+        public List<Employee> GetEmployees() => new List<Employee>
+        {
+            new Employee{ EmpId = 1, EmpName="Pavan R", EmpAddress="Bangalore", EmpSalary = 25000},
+            new Employee{ EmpId = 2, EmpName="Sachin R", EmpAddress="Mysore", EmpSalary = 28000},
+            new Employee{ EmpId = 3, EmpName="Vishwas V", EmpAddress="Chikka", EmpSalary = 25000},
+            new Employee{ EmpId = 4, EmpName="Sawai K", EmpAddress="Andhra", EmpSalary = 29000},
+            new Employee{ EmpId = 5, EmpName="Akshatha H", EmpAddress="Hakkandi", EmpSalary = 28000},
+            new Employee{ EmpId = 6, EmpName="Roja", EmpAddress="Mandya", EmpSalary = 27000},
+        };
+    }
+}
